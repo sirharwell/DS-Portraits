3861dd2f61de6d8bc9d125b77474fa0af3bc7cee
Ian Harwell <sirharwell@gmail.com>
2018-07-31 00:12:09 -0600

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Parallax, Background } from 'react-parallax';
+
 
 class App extends Component {
   render() {
     return (
+      <Parallax
+        blur={10}
+        bgImage={require('path/to/image.jpg')}
+        bgImageAlt="the cat"
+        strength={200}
+      >
+        Put some text content here
+        or even an empty div with fixed dimensions
+        to have a height for the parallax.
+        <div style={{ height: '200px' }} />
+      </Parallax>
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={require('path/to/another/image.jpg')}
+        bgImageAlt="the dog"
+        strength={200}
+      >
+        Blur transition from min to max
+        <div style={{ height: '200px' }} />
+      </Parallax>
+      <Parallax strength={300}>
+        <div>Use the background component for custom elements</div>
+        <Background className="custom-bg">
+          <img src="http://www.fillmurray.com/500/320" alt="fill murray" />
+        </Background>
+      </Parallax>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
