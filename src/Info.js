d93e217f6b1aa54ca272d67ff35f8b871ff6c6a0
Ian Harwell <sirharwell@gmail.com>
2018-08-01 23:10:35 -0600

    info

---
+++ src/Info.js
@@ -0,0 +1,12 @@
+Information
+Contact us
+Map to the studio, phone number, email, etc
+
+About us
+Get to know us. See what we do when we're not photographing people, we might have things in common!
+
+Clothing advice
+Trust us, you really don't want to wear plaids. Good information to read.
+
+Investment
+What can you expect to pay?
